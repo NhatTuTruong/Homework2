a242d7b54c886b34b0c30e019c61cf328763cea6
NhatTuTruong <nhatdt2020@gmail.com>
2023-11-10 15:47:23 +0700

    Create Source

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import React from 'react';
+
+function App() {
+
+
+  return (
+    <>
+     <div className="bg-blue-500 text-white p-4">
+      Hello, Tailwind CSS!
+    </div>
+    </>
+  )
+}
+
+export default App
